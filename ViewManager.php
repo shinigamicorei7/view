a4c5c3edd8eef825562b93bb269d3c57075a857d
shinigamicorei7 <bryan_slvr@hotmail.com>
2015-08-22 14:02:55 -0500

    Realizamos varias mejoras

--- ViewManager.php
+++ ViewManager.php
@@ -1,6 +1,10 @@
 <?php namespace Matrix\View;
 
-define('VIEW_DIR', __DIR__);
+use Twig_Environment;
+use Twig_Loader_Filesystem;
+use Twig_LoaderInterface;
+
+define('LIB_DIR', __DIR__);
 
 class ViewManager
 {
@@ -11,23 +15,34 @@ class ViewManager
     protected static $instance = null;
 
     /**
-     * @var \Twig_Environment
+     * @var Twig_Environment
      */
-    protected $twig;
+    protected $environment;
+    /**
+     * @var Twig_LoaderInterface
+     */
+    protected static $loader = null;
 
     /**
      * @var array
      */
-    protected $config;
+    protected static $config;
+    protected $defaultConfig = 'config/view.php';
 
     /**
      * ViewManager constructor.
+     * @param Twig_LoaderInterface $loaderInterface
+     * @internal param array $config
      */
-    public function __construct()
+    public function __construct(Twig_LoaderInterface $loaderInterface = null)
     {
-        $this->config = require 'config/view.php';
-        $this->loadTwigEnvironment();
-        self::setInstance($this);
+        if (is_null(self::$config)) {
+            self::setDirConfig($this->defaultConfig);
+        }
+
+        if (!is_null($loaderInterface)) {
+            self::setLoader($loaderInterface);
+        }
     }
 
     /**
@@ -46,18 +61,60 @@ class ViewManager
         self::$instance = $instance;
     }
 
-    protected function loadTwigEnvironment()
+    public function loadTwigEnvironment()
     {
-        $loader = new \Twig_Loader_Filesystem($this->config['views_dir']);
-        $this->twig = new \Twig_Environment($loader);
+        $loader = self::getLoader();
+
+        $this->setEnvironment(new Twig_Environment($loader, self::$config['options']));
     }
 
     /**
-     * @return \Twig_Environment
+     * @return Twig_Environment
      */
-    public function getTwig()
+    public function getEnvironment()
     {
-        return $this->twig;
+        return $this->environment;
+    }
+
+    /**
+     * @return Twig_LoaderInterface
+     */
+    public static function getLoader()
+    {
+        if (is_null(self::$loader)) {
+            self::$loader = new Twig_Loader_Filesystem(self::$config['templates_dir']);
+            if (!is_null(self::$config['namespaces']) && is_array(self::$config['namespaces'])) {
+                foreach (self::$config['namespaces'] as $namespace => $dir) {
+                    self::$loader->addPath($dir, $namespace);
+                }
+            }
+        }
+
+        return self::$loader;
+    }
+
+    /**
+     * @param Twig_LoaderInterface $loader
+     */
+    public static function setLoader(Twig_LoaderInterface $loader)
+    {
+        self::$loader = $loader;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getConfig()
+    {
+        return self::$config;
+    }
+
+    /**
+     * @param string $config
+     */
+    public static function setDirConfig($config)
+    {
+        self::$config = require $config;
     }
 
     public static function __callStatic($name, $arguments)
@@ -66,12 +123,25 @@ class ViewManager
 
         if (is_null($instance)) {
             $instance = new ViewManager();
+            $instance->loadTwigEnvironment();
+            ViewManager::setInstance($instance);
         }
 
-        $twig = $instance->getTwig();
+        $environment = $instance->getEnvironment();
 
-        return call_user_func_array(array($twig, $name), $arguments);
+        return call_user_func_array(array($environment, $name), $arguments);
 
+    }
+
+    public function __call($name, $arguments)
+    {
+        $environment = $this->getEnvironment();
+        return call_user_func_array(array($environment, $name), $arguments);
+    }
+
+    public function setEnvironment(Twig_Environment $environment)
+    {
+        $this->environment = $environment;
     }
 
 
