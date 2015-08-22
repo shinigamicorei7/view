3c6ea038c6373f055610bdc616e9e1c72017a1e2
shinigamicorei7 <bryan_slvr@hotmail.com>
2015-08-22 14:03:54 -0500

    Definimos una configuración avanzada

--- config/view.php
+++ config/view.php
@@ -1,5 +1,28 @@
 <?php
 
 return array(
-    "views_dir" => VIEW_DIR . '/views/'
+    /**
+     * Este campo es obligatorio ya que se usará como directorio root de las plantillas
+     *
+     * templates_dir string|array
+     */
+    'templates_dir' => array(
+        LIB_DIR . '/views/'
+    ),
+    /**
+     * Namespaces que se usaran en la aplicación Ej:
+     *
+     * echo view('@namespace/index.twig',$data);
+     * o
+     * echo View::render('@namespace/index.twig',$data);
+     *
+     * namespaces null|array
+     */
+    'namespaces' => array(
+        'test' => LIB_DIR . '/pruebas/'
+    ),
+    'options' => array(
+        'debug' => false,
+        'cache' => LIB_DIR . '/cache/'
+    )
 );
